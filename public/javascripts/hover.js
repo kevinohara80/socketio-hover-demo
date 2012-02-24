425aa6cb357195c8d046785f8b2f08a30eb17e8b
Kevin O'Hara <kevinohara80@gmail.com>
2012-02-23 20:45:49 -0500

    updating for heroku

--- public/javascripts/hover.js
+++ public/javascripts/hover.js
@@ -1,6 +1,6 @@
 (function($, io) {
   
-  var socket = io.connect('http://localhost:3000');
+  var socket = io.connect('http://sharp-flower-2982.herokuapp.com/');
   
   socket.on('connect', function () {
       socket.on('data', function(data){
