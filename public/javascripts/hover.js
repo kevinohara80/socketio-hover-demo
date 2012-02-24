9bc0993159bd386d7792e7a737f1a74e929dce64
Kevin O'Hara <kevinohara80@gmail.com>
2012-02-23 20:54:33 -0500

    updated to work on document load

--- public/javascripts/hover.js
+++ public/javascripts/hover.js
@@ -1,18 +1,27 @@
 (function($, io) {
   
-  var socket = io.connect('http://sharp-flower-2982.herokuapp.com/');
-  
-  socket.on('connect', function () {
-      socket.on('data', function(data){
-        $('.results ul').append('<li>' + data + '</li>');
-      });
-  });
+  var socket; 
   
   $(document).ready(function(){
-    $('.box').mouseover(function(){
-      var txt = $(this).children().first('p').text();
-      socket.emit('hover', txt);
+    
+    socket = io.connect('http://sharp-flower-2982.herokuapp.com/');
+    //socket = io.connect('http://localhost:3000');
+    
+    socket.on('connect', function () {
+        
+        $('.status').text('Status: Connected!');
+        
+        $('.box').mouseover(function(){
+          var txt = $(this).children().first('p').text();
+          socket.emit('hover', txt);
+        });
+        
+        socket.on('data', function(data){
+          $('.results ul').append('<li>' + data + '</li>');
+        });
+        
     });
+    
   });
   
 })(jQuery, io);
