4b2280cbd0e2ca5c82783d89a9420b8995ca76e4
Kevin O'Hara <kevinohara80@gmail.com>
2012-03-02 13:33:11 -0500

    added socket listeners

--- public/javascripts/hover.js
+++ public/javascripts/hover.js
@@ -2,30 +2,44 @@
   
   var socket; 
   
+  var consoleWrite = function(msg) {
+    var height = $('.results ul').height();
+    $('.results ul').append('<li> &gt; ' + msg + '</li>');
+    $('.results').scrollTop(height);
+  }
+  
   $(document).ready(function(){
     
     $('.results ul').append('<li> &gt; Connecting to server...</li>');
     
     socket = io.connect(window.location);
     
+    socket.on('connnecting', function(transport) {
+      consoleWrite('Trying transport: ' + transport);
+    });
+    
     socket.on('connect', function () {
-        
       $('.box').mouseover(function(){
         $(this).addClass('green');
         var txt = $(this).children().first('p').text();
         socket.emit('hover', txt);
       }).mouseout(function(){
         $(this).removeClass('green');
-      });
-        
-      socket.on('data', function(data){
-        var height = $('.results ul').height();
-        $('.results ul').append('<li> &gt; ' + data + '</li>');
-        $('.results').scrollTop(height);
-      });
-        
+      });   
     });
-
+    
+    socket.on('data', function(data){
+      consoleWrite(data);
+    });
+    
+    socket.on('disconnect', function(){
+      consoleWrite('Disconnected from the server!');
+    });
+    
+    socket.on('connect_failed', function(){
+      consoleWrite('Connection to the server failed!');
+    });
+    
   });
   
 })(jQuery);
